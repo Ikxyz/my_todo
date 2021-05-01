01f99b7890319d339d292218ec0eea7d81c406d3
CyberGA <etomu.joshua@gmail.com>
2021-05-01 15:06:28 +0100

    added javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+const func1 = () => {
+  console.log("Hello World!!");
+  return 'Hello World!!'
+};
+const greetUser = (name) => {
+  console.log("Hello", name);
+  return `Hello ${name}`
+};
+
+func1();
+greetUser("Gbenga")
+
+// console.log(func1());
+// console.log(greetUser("Gbenga"));
