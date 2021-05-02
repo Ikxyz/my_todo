cd5e5038bd04145932e8953c5e6f992233a6e32d
Tedionwe <69795021+Tedionwe@users.noreply.github.com>
2021-05-02 20:01:50 +0100

    adding my javascript assignment

--- ll
+++
@@ -1,8 +0,0 @@
-write a function that returns hello world 
-write a function that takes in an input and returns hello + that input
-
-
- grid-column-start: 1;
-  grid-column-end: 2;
-  grid-row-start: 2;
-  grid-row-end: 4;
---
+++ script.js
@@ -0,0 +1,11 @@
+// write a function that returns hello world 
+// write a function that takes in an input and returns hello + that input
+
+
+function hello(){
+ console.log("Hello World")
+}
+
+function helloYou(name){
+    console.log("Hello "+name)
+}
