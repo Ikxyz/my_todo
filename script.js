2ae148cdc9ce1d10795477880d2c459390bc2099
Hope Crystal <hope.orobosa.henry@gmail.com>
2021-05-02 21:26:12 +0100

    redo

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function addItem() {
     target.innerHTML += "<div class='todo-item'><span>"+ userInput +"</span><i class='fas fa-minus'></i></div>";
 }
 
+// function oga() {
+//     var itemClass = document.getElementsByClassName('todo-item');
+//     for (var i = 0; i < itemClass.length; i++ ) {
+//         itemClass[i].id = "bobo";
+//     }
+// }
 
 // Remove an Item
 function removeItem() {
