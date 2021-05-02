b25ed31c3692506e3bf5d9f7a7d2b1b32ce94aaa
Chinonye Dim <nornisempire@Chinonyes-MacBook-Pro.local>
2021-05-02 22:54:12 +0100

    update

---
+++ Index.js
@@ -0,0 +1,11 @@
+ 
+ 
+function greet() {
+   console.log('Hello World');
+}
+   greet();
+
+   function greetTakeInInput(name) {
+       console.log('Hello' + " " + name );
+    }
+       greetTakeInInput('Josh');
---
+++ greet.js
@@ -0,0 +1,9 @@
+function greet() {
+    console.log('Hello World');
+ }
+    greet();
+
+    function greetTakeInInput(name) {
+        console.log('Hello' + " " + name );
+     }
+        greetTakeInInput('Josh');
