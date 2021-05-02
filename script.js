f3e079c8996956150200e650503db570aa34b4b9
Hope Crystal <hope.orobosa.henry@gmail.com>
2021-05-02 22:54:00 +0100

    redo

--- script.js
+++ script.js
@@ -3,18 +3,20 @@
 function addItem() {
     let userInput = document.getElementById("userInput").value;
     let target = document.getElementById("todoItems");
-    target.innerHTML += "<div class='todo-item'><span>"+ userInput +"</span><i class='fas fa-minus'></i></div>";
+    target.innerHTML += "<div class='todo-item'><span>"+ userInput +"</span><i class='fas fa-minus' id='remove' onclick='removeItem()'></i></div>";
 }
 
-// function oga() {
-//     var itemClass = document.getElementsByClassName('todo-item');
-//     for (var i = 0; i < itemClass.length; i++ ) {
-//         itemClass[i].id = "bobo";
-//     }
-// }
+function oga() {
+    var itemClass = document.getElementsByClassName('todo-item');
+    for (var i = 0; i < itemClass.length; i++ ) {
+        itemClass[i].id = "bobo";
+    }
+}
 
 // Remove an Item
 function removeItem() {
-    let remove = document.getElementById("remove")
-    remove.style.display = "none"
+    document.getElementById("remove").parentElement.remove()
+
+    // let remove = document.getElementById("remove")
+    // remove.remove()
 }
