6637ebc8b8ee1f24c00467c6c92594ae2b774707
Hope Crystal <hope.orobosa.henry@gmail.com>
2021-05-02 12:14:53 +0100

    redo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function addItem() {
-    var userInput = document.getElementById("userInput").value;
-    var target = document.getElementById("todoItems");
+    let userInput = document.getElementById("userInput").value;
+    let target = document.getElementById("todoItems");
     target.innerHTML += "<div class='todo-item'><span>"+ userInput +"</span><i class='fas fa-minus'></i></div>";
 }
