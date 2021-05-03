47e3bcd0373926332bfe5184b1ab97fb62e891ad
Hope Crystal <hope.orobosa.henry@gmail.com>
2021-05-03 08:21:25 +0100

    project

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-function writeName() {
-    
+function hello() {
+    return "Hello World!"
 }
+
+
+function userInput(name){ 
+    if(name){ 
+        return "Hello, " + name + "!"; 
+    }else{ 
+        return "Hello there!"; 
+    } 
+}
+
+
+console.log(hello())
+console.log(userInput("Poppy"))
