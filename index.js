2b273e7a5e830a69ac3a4d4f4d6ca37c369a5fdb
Rumwen1 <rumwen.efe@gmail.com>
2021-05-03 14:14:16 +0100

    function

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ console.log(welcome);
 let Name ='Rumwen';
 let greeting = welcome + ' ' + Name;
 console.log(greeting);
+
+
+function greet(){
+    console.log('Hello World');
+}
+greet ();
+
+function greet(name){
+    console.log('Hello ' + name);
+}
+
+greet('Rumwen');
