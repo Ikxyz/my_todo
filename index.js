47d1695c59c6e309d16a140f2bec369cf7af25f6
Rumwen1 <rumwen.efe@gmail.com>
2021-05-06 11:27:34 +0100

    my-homework

--- index.js
+++ index.js
@@ -1,18 +1,106 @@
-let welcome = 'Hello';
-console.log(welcome);
+// write a function that returns hello world 
+// write a function that takes in an input and returns hello + that input
+// function start(){
+//     console.log("Hello World!")
+// }
 
-let Name ='Rumwen';
-let greeting = welcome + ' ' + Name;
-console.log(greeting);
+// start();
+// function greet(name){
+//     console.log('hello' + ' ' + name);
+// }
+// greet('Victory');
+console.log("Todo v 1.0.0")
 
+let listOfTodo = [];
 
-function greet(){
-    console.log('Hello World');
+let timeouts = {};
+
+const getElement= (id)=> document.getElementById(id);
+
+function addTodo(todo){
+    if(!todo)return; 
+
+    if (listOfTodo.includes(todo))return;
+
+    listOfTodo.push(todo);
+        
+    getElement("list-of-todos").innerHTML += todoItem(todo);
+    
+    localStorage.setItem("todos", JSON.stringify(listOfTodo));
+
+    onNewTodoAdded();
 }
-greet ();
 
-function greet(name){
-    console.log('Hello ' + name);
+getElement("input-todo").value="";
+
+function loadFormStorage() {
+    const todos = localStorage.getItem("todos");
+    
+  if (!todos) return;
+
+  JSON.parse(todos).map((todo) => {
+    addTodo(todo);
+  });
+
 }
 
-greet('Rumwen');
+function onTodoInputChange(event, todo) {    
+    if (event.target.checked === true) {
+        timeouts[todo] = setTimeout(() => {
+          removeTodo(todo);
+        }, 3 * 1000 );
+      }
+    
+      if (event.target.checked === false) {
+        clearTimeout(timeouts[todo]);
+        timeouts[todo] = 0;
+      }
+         getElement(todo).classList.toggle("completed");
+         
+         
+        }
+
+function onNewTodoAdded() {
+   listOfTodo.forEach((todo) => {
+    getElement("input" + todo).addEventListener("change", (event)=>
+    onTodoInputChange(event,todo));
+   });
+}
+
+function removeTodo(todo){
+    listOfTodo = listOfTodo.filter((e)=> todo!=e);
+    const todoElement = getElement(todo);
+
+    if(todoElement){
+        todoElement.remove();
+    }
+}
+
+function onClickAddTodoButton(){
+    const newTodo = getElement("input-todo").value
+    addTodo(newTodo);
+}
+
+function onCheckboxStateChange(event){
+      
+}
+
+function todoItem(todo){
+    return `
+    <div id="${todo}" class="d-flex justify-content-between todo-item ">
+    <span>${todo}</span>
+    <input id="${'input'+todo}" class="todo-checkbox" type="checkbox" />
+    </div>
+    `;
+} 
+
+getElement("input-todo").addEventListener("keydown",(event)=>{
+    if(event.code === "Enter"){
+      onClickAddTodoButton()
+    }
+  })
+
+  
+localStorage.removeItem("todos");
+loadFormStorage();
+
