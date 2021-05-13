52b21df24a63fd6e19e3f4d9ce163c3a136ba3af
Hope Crystal <hope.orobosa.henry@gmail.com>
2021-05-13 21:38:05 +0100

    Todo App v2.0.1

--- script.js
+++ script.js
@@ -3,47 +3,42 @@ var form = document.getElementById("addForm");
 var itemList = document.getElementById("items");
 
 // Form submit event
-form.addEventListener('submit', addItem);
-
+form.addEventListener("submit", addItem);
 
 function addItem(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    // Get input value
-    var newItem = document.getElementById('inputItem').value;
+  // Get input value
+  var newItem = document.getElementById("inputItem").value;
 
-    // Create new li with class
-    var li = document.createElement('li');
-    li.className = 'todo-item';
-    
-    // Add text node with input value
-    var createText = document.createTextNode(newItem)
-    li.appendChild(createText);
+  // Create new li with class
+  var li = document.createElement("li");
+  li.className = "todo-item";
 
-    // Create the delete buttonwithlass and append to li
-    var delButton = document.createElement('button')
-    delButton.className = 'btn btn-danger btn-sm delete'
-    delButton.appendChild(document.createTextNode('X'))
-    li.appendChild(delButton)
+  // Add text node with input value
+  var createText = document.createTextNode(newItem);
+  li.appendChild(createText);
 
-    itemList.appendChild(li)
+  // Create the delete buttonwithlass and append to li
+  var delButton = document.createElement("button");
+  delButton.className = "btn btn-danger btn-sm delete";
+  delButton.appendChild(document.createTextNode("X"));
+  li.appendChild(delButton);
+
+  itemList.appendChild(li);
 }
 
 // Delete button event
-itemList.addEventListener('click', removeItem)
+itemList.addEventListener("click", removeItem);
 
 function removeItem(e) {
-    if (e.target.classList.contains('delete')) {
-        if (confirm("Are you sure?")) {
-            var li = e.target.parentElement;
-            itemList.removeChild(li)
-        }
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are you sure?")) {
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
     }
-    
+  }
 }
-
-
-
 
 // v1.0.2
 /* const submitForm = document.querySelector('.add');
